8d085360063ed1efd665b551b85006870f1c51a4
yukio <143847357+yukio-kayaba@users.noreply.github.com>
2024-12-03 15:15:31 -0500

    quitando secion

--- adm_hotel_sky/index.php
+++ adm_hotel_sky/index.php
@@ -7,39 +7,39 @@
 	require_once("controller/controlador_vista.php");
 	session_start();
 	// print_r($segmentos);
-	if(isset($_SESSION['id'])){
-		$segmentos = isset($_GET['vista']) ? explode('/', $_GET['vista']):"";
-		if(isset($segmentos[1])){
-			if(method_exists("ModeloControladorVista",$segmentos[1])){
-				// if (isset($segmentos[1]) && $segmentos[0] == 'panel') {
-				// 	if(method_exists('ModeloControladorVista',$segmentos[1])){
-				// 		$_SESSION['archivo'] = $segmentos[1];
-				// 		ModeloControlador::archivo();
-				// 	}else{
-				// 		ModeloControlador::error_pagina();
-				// 	}
-				// }else{
-				// 	ModeloControlador::{$segmentos[0]}();
-				// }
-				$_SESSION['archivo'] = $segmentos[1];
-				ModeloControlador::archivo();
-			} else {
-				if(method_exists("ModeloControlador",$segmentos[0])){
-					ModeloControlador::{$segmentos[0]}();
-				}else{
-					ModeloControlador::error_pagina();
-				}
-			}
-		}else {
-			if(isset($segmentos[0])){
-				if(method_exists("ModeloControlador",$segmentos[0])){
-					ModeloControlador::{$segmentos[0]}();
-				}
+	$segmentos = isset($_GET['vista']) ? explode('/', $_GET['vista']):"";
+	if(isset($segmentos[1])){
+		if(method_exists("ModeloControladorVista",$segmentos[1])){
+			// if (isset($segmentos[1]) && $segmentos[0] == 'panel') {
+			// 	if(method_exists('ModeloControladorVista',$segmentos[1])){
+			// 		$_SESSION['archivo'] = $segmentos[1];
+			// 		ModeloControlador::archivo();
+			// 	}else{
+			// 		ModeloControlador::error_pagina();
+			// 	}
+			// }else{
+			// 	ModeloControlador::{$segmentos[0]}();
+			// }
+			$_SESSION['archivo'] = $segmentos[1];
+			ModeloControlador::archivo();
+		} else {
+			if(method_exists("ModeloControlador",$segmentos[0])){
+				ModeloControlador::{$segmentos[0]}();
 			}else{
-				ModeloControlador::panel();
+				ModeloControlador::error_pagina();
 			}
-		};
-	}else{
-		ModeloControlador::loguin();
-	}
+		}
+	}else {
+		if(isset($segmentos[0])){
+			if(method_exists("ModeloControlador",$segmentos[0])){
+				ModeloControlador::{$segmentos[0]}();
+			}
+		}else{
+			ModeloControlador::panel();
+		}
+	};
+	// if(isset($_SESSION['id'])){
+	// }else{
+	// 	ModeloControlador::loguin();
+	// }
 ?>
