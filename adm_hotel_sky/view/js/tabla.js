675e4fd38b21eddc9ef868c808a978c027e374db
yukio <zviamontevilca@gmail.com>
2024-12-09 21:43:02 -0500

    habilitando el pop up
    se esta habilitando el pop up

--- adm_hotel_sky/view/js/tabla.js
+++ adm_hotel_sky/view/js/tabla.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
                         valores_title += ` <th> ${element}</th>`;
                     }
                 });
-
+                
                 valores_title += ` <th>Opciones</th>`;
                 document.getElementsByClassName('titulos_date')[0].innerHTML = valores_title;
                 let datos_subir = "";
@@ -43,9 +43,6 @@ $(document).ready(function(){
                     });
                     datos_subir += `
                         <td class="actions">
-                                <button class="btn-view" data-id="1">
-                                    <i class="fas fa-eye"></i>
-                                </button>
                                 <button class="btn-edit" data-id="1">
                                     <i class="fas fa-edit"></i>
                                 </button>
@@ -62,6 +59,68 @@ $(document).ready(function(){
         })
     }
 
+
     carga_valores(0);
 
+    // funciones del pop up
+    const fondo_back_up = document.getElementById("pop_up");
+    const cuerpo_pop_up = document.getElementById("cuerpo_pop_up");
+    const boton_add = document.getElementById("boton_add");
+    const boton_add_enlaces = document.getElementsByClassName("boton_add_enlaces")[0];
+    fondo_back_up.addEventListener("click",(e)=>{
+        // e.stopPropagation(); 
+        fondo_back_up.style.display = "none";
+    });
+    cuerpo_pop_up.addEventListener("click",(e)=>{
+        e.stopPropagation();
+    });
+    boton_add.addEventListener("click",()=>{
+        let contenedor = document.getElementById("contenedor_principal_pop");
+        let texto = "";
+        let campos = Object.keys(datos_global[0]);
+        campos.forEach(element => {
+            if(element  == "caracteristicas" || element == "precio"){
+                texto += etiquetas_datos(element);
+            }
+        });
+        contenedor.innerHTML = texto;
+        fondo_back_up.style.display = "flex";
+    });
+    boton_add_enlaces.addEventListener("click",()=>{
+        const campo_fotos_enlaces = document.getElementById("fotos_enlaces");
+        let texto = "";
+        texto = campo_fotos();
+        campo_fotos_enlaces.innerHTML += texto;
+    });
 });
+
+function etiquetas_datos(titulo,dato=null){
+
+    let texto = "";
+    texto = `
+        <div class="field">
+                <label class="label">${titulo}</label>
+                <div class="control has-icons-left has-icons-right">
+                    <input class="input is-success" type="text" placeholder="Ingrese el ${titulo}" value="${(dato==null)?"":dato}">
+                    <span class="icon is-small is-left">
+                    <i class="fas fa-user"></i>
+                    </span>
+                    <span class="icon is-small is-right">
+                    <i class="fas fa-check"></i>
+                    </span>
+                </div>
+            </div>
+    `;
+    return texto;
+};
+
+function campo_fotos(enlace = null){
+    let texto = "";
+    texto = `
+        <div class="control input_imagen">
+            <input class="input" type="text" placeholder="Ingrese el enlace" value="${(enlace != null)? enlace:""}">
+            <img width="150px" src="${(enlace != null)? enlace:'https://images.pexels.com/photos/12296667/pexels-photo-12296667.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'}" alt="">
+        </div>
+    `;
+    return texto;
+};
--- adm_hotel_sky/view/tabla.php
+++ adm_hotel_sky/view/tabla.php
@@ -5,7 +5,8 @@
 <?php $titulo = "VILLA | Sistema $titulo1 "; ?>
 <?php include("layouts/head.php"); ?>
     <link rel="stylesheet" href="<?php echo url; ?>/view/css/tabla.css">
-   
+    <link rel="stylesheet" href="<?php echo url; ?>/view/css/pop-up.css">
+    
     <script src="<?php echo url;?>/view/js/menu-responsive.js" defer></script>
     <script src="<?php echo url;?>/view/js/tabla.js" defer></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
@@ -25,8 +26,12 @@
     <!-- Contenido principal -->
     <main class="main-content">
         <div class="dashboard-content">
-            <div class="welcome-section2">
+            <div class="welcome-section2" style="display: flex;align-items: center;gap: 15px; ">
                 <h1>Gestión de <?php echo $titulo1; ?></h1>
+                <div id="boton_add" style="display: flex;flex-direction: column;cursor: pointer; " title="Agregar mas <?php echo $titulo1; ?>">
+                    <svg width="35px" height="35px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M15 12L12 12M12 12L9 12M12 12L12 9M12 12L12 15" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> <path d="M7 3.33782C8.47087 2.48697 10.1786 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 10.1786 2.48697 8.47087 3.33782 7" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> </g></svg>
+                    <p style="font-size: 15px;">agregar</p>    
+                </div>
             </div>
             
             <div class="table-container">
@@ -63,5 +68,8 @@
             </div>
         </div>
     </main>
+    <div id="pop_up">
+        <?php include("view/layouts/form_modal.php"); ?>
+    </div>
 </body>
 </html>
